e1d39e3909ed0856c445edbc8addf3db9d451c26
Alexsandro Santos <web_alexsandro@hotmail.com>
2023-08-03 21:58:31 -0300

    Excluded clients table and included phone column in users table

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,6 +1,15 @@
+import Client from '../models/Client';
+
 class HomeController {
   async index(req, res) {
-    res.status(200).json('index');
+    // res.status(200).json('index');
+    const newClient = await Client.create({
+      name: 'Alexsandro',
+      surname: 'Vieira dos Santos',
+      phone: '11945305889',
+      email: 'web_alexsandro@hotmail.com',
+    });
+    res.json(newClient);
   }
 }
 
--- src/database/index.js
+++ src/database/index.js
@@ -1,9 +1,10 @@
 import Sequelize from 'sequelize';
 import databaseConfig from '../config/database';
 import Item from '../models/Item';
+import Client from '../models/Client';
 import User from '../models/User';
 
-const models = [Item, User];
+const models = [Client, Item, User];
 
 const connection = new Sequelize(databaseConfig);
 
