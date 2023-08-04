9641c8a691011f873f406c3b39c56553e01cc172
Alexsandro Santos <web_alexsandro@hotmail.com>
2023-08-03 22:38:58 -0300

    Revert "Excluded clients table and included phone column in users table"
    This reverts commit e1d39e3909ed0856c445edbc8addf3db9d451c26.

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,15 +1,6 @@
-import Client from '../models/Client';
-
 class HomeController {
   async index(req, res) {
-    // res.status(200).json('index');
-    const newClient = await Client.create({
-      name: 'Alexsandro',
-      surname: 'Vieira dos Santos',
-      phone: '11945305889',
-      email: 'web_alexsandro@hotmail.com',
-    });
-    res.json(newClient);
+    res.status(200).json('index');
   }
 }
 
